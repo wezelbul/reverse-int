1de04a60cd153feb21bab72cfa802fafe1c71737
wezelbul <d.chesnokov.01@yandex.ru>
2023-07-31 14:14:14 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let strNum = n.toString()
+
+    const numLength = strNum.length;
+    for (let i = numLength - 1; i >= 0; i--) {
+        strNum += strNum[i];
+    }
+    return parseInt(strNum.substring(numLength));
 }
